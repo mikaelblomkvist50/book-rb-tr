96e149655cf3f1555a7080fb15114a804241c0da
mikaelblomkvist50 <mikaelblomkvist50@gmail.com>
2018-05-31 22:52:17 +1000

    mooooooooooooooooooving.

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,5 +1,31 @@
-module ToyRobotRyanBigg
+module ToyRobot
   class Robot
+    # attr_reader :east
+    attr_reader :north
 
+    def initialize(east = 0, north = 0)
+      @east = east
+      @north = north
+    end
+
+    def east
+      @east
+    end
+
+    def move_east
+      @east+= 1
+    end
+
+    def move_west
+      @east -= 1
+    end
+
+    def move_north
+      @north += 1
+    end
+
+    def move_south
+      @north -= 1
+    end
   end
 end
--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -3,11 +3,53 @@ require 'spec_helper'
 #We've refered to the Robot class under the ToyRobot module as our application is called ToyRobot.
 #And the container for the robot logic is going to be the Robot class.
 describe ToyRobot::Robot do
-  subject { ToyRobot::Robot.new(0) }
+  subject { ToyRobot::Robot.new(0) }                                   #=> Given
 
-  it 'moves 3 spaces' do
-    3.times { subject.move}
+  it 'moves 3 spaces east' do
+    3.times { subject.move_east}                                       #=> When
 
-    expect(subject.position).to eq(3)
+    expect(subject.east).to eq(3)                                      #=> Then
+  end
+
+  it 'moves 4 spaces east' do
+    4.times { subject.move_east}                                       #=> When
+
+    expect(subject.east).to eq(4)                                      #=> Then
+  end
+
+  it 'moves 3 spaces west' do
+    3.times { subject.move_west}                                       #=> When
+
+    expect(subject.east).to eq(-3)                                     #=> Then
+  end
+
+  it 'moves 4 spaces west' do
+    4.times { subject.move_west}                                       #=> When
+
+    expect(subject.east).to eq(-4)                                     #=> Then
+  end
+
+  it 'moves 3 spaces north' do
+    3.times { subject.move_north}                                      #=> When
+
+    expect(subject.north).to eq(3)                                     #=> Then
+  end
+
+  it 'moves 4 spaces east' do
+    4.times { subject.move_north}                                      #=> When
+
+    expect(subject.north).to eq(4)                                     #=> Then
+  end
+
+  it 'moves 3 spaces south' do
+    3.times { subject.move_south}                                      #=> When
+
+    expect(subject.north).to eq(-3)                                    #=> Then
+  end
+
+  it 'moves 4 spaces south' do
+    4.times { subject.move_south}                                      #=> When
+
+    expect(subject.north).to eq(-4)                                    #=> Then
   end
 end
